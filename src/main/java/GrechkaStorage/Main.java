7e26534b4de1d4ea2681eabb5ad718db7914308a
Dmitriy <Dmitriy.Neganov@vsegda-da.com>
2023-11-06 17:33:22 +0400

    lesson three

---
+++ src/main/java/GrechkaStorage/Main.java
@@ -0,0 +1,29 @@
+package GrechkaStorage;
+
+import java.io.InputStream;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int eatsPerMonth = 6;
+        System.out.print("Введите количество месяцев: ");
+        int month = scanner.nextInt();
+        int monthLeft = month;
+        int buckwheatWeightLeft = month * eatsPerMonth;
+        int totalBuckwheatWeight = month * eatsPerMonth;
+        int totalMoneyForStorage = 0;
+        final int moneyForStoragePerMonthForOneKg = 100;
+        for (int i = 1; i <= month; i++){
+            totalMoneyForStorage = totalMoneyForStorage + buckwheatWeightLeft * moneyForStoragePerMonthForOneKg;
+            buckwheatWeightLeft = buckwheatWeightLeft - eatsPerMonth;
+            monthLeft--;
+        }
+        System.out.println("Василий хранит свои запасы гречки на складе. \n" +
+                "Василий примерно съедает примерно " + eatsPerMonth + "кг гречки. Всего гречки у Василия " +
+                totalBuckwheatWeight + "кг. \n" +
+                "Платеж за хранение 1 килограмма гречки составляет " + moneyForStoragePerMonthForOneKg + "руб./месяц. \n" +
+                totalMoneyForStorage + " рублей - поднадобится залпатить Василию, для хранения гречки, до исчерпания его запасов ");
+
+    }
+}
